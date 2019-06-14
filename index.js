a9bee43f1d2b8a8cc28840bd6688fc2ea52cdbce
kevin <kevin.nin92@gmail.com>
2019-06-14 19:23:46 +0530

    refactor: move server related taks to server file and worker related tasks to worker file

--- index.js
+++ index.js
@@ -1,86 +1,24 @@
 /*
- *Primary file for the api
+ * primary file for api
  */
 
-const http = require('http');
-const https = require('https');
-const fs = require('fs');
-const url = require('url');
-const StringDecoder = require('string_decoder').StringDecoder;
-const config = require('./config');
-const handlers = require('./lib/handlers');
-const helpers = require('./lib/helpers');
+//dependencies
+const server = require('./lib/server');
+const workers = require('./lib/workers');
 
-const httpServer = http.createServer(function(req, res) {
-  unifiedServer(req, res);
-});
+//declare app
+const app = {};
 
-httpServer.listen(config.httpPort, () => {
-  console.log('Server started at port :', config.httpPort, ' in', config.envName);
-});
-
-const httpsServerOptions = {
-  key: fs.readFileSync('./https/key.pem'),
-  cert: fs.readFileSync('./https/cert.pem')
+//init function
+app.init = function() {
+  //start server
+  server.init();
+  //start worker
+  workers.init();
 };
 
-const httpsServer = https.createServer(httpsServerOptions, function(req, res) {
-  unifiedServer(req, res);
-});
+//execute
+app.init();
 
-httpsServer.listen(config.httpsPort, () => {
-  console.log('Server started at port :', config.httpsPort, ' in', config.envName);
-});
-
-const unifiedServer = (req, res) => {
-  const parsedUrl = url.parse(req.url, true);
-  const path = parsedUrl.pathname;
-  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
-  const method = req.method.toLowerCase();
-  const queryStringObject = parsedUrl.query;
-  const headers = req.headers;
-
-  //parsing payload
-  const decoder = new StringDecoder('utf-8');
-  let buffer = '';
-
-  req.on('data', (data) => {
-    buffer += decoder.write(data);
-  });
-
-  req.on('end', () => {
-    buffer += decoder.end();
-
-    const chosenHandler =
-      typeof router[trimmedPath] !== 'undefined' ? router[trimmedPath] : handlers.notFound;
-
-    const data = {
-      trimmedPath,
-      queryStringObject,
-      method,
-      headers,
-      payload: helpers.parseJsonToObject(buffer)
-    };
-
-    chosenHandler(data, function(statusCode, payload) {
-      statusCode = typeof statusCode === 'number' ? statusCode : 200;
-
-      payload = typeof payload === 'object' ? payload : {};
-
-      const payloadString = JSON.stringify(payload);
-
-      res.setHeader('Content-Type', 'application/json');
-      res.writeHead(statusCode);
-      res.end(payloadString);
-
-      console.log('Returning', statusCode, payloadString);
-    });
-  });
-};
-
-const router = {
-  ping: handlers.ping,
-  users: handlers.users,
-  tokens: handlers.tokens,
-  checks: handlers.checks
-};
+//export app
+module.exports = app;
